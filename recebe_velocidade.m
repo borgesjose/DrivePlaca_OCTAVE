87c5f945d03907cd6f1975808eefdb62ff329143
borgesjose <jose.borges90@hotmail.com>
2022-06-16 10:45:35 -0300

    16-06-2022 version

--- recebe_velocidade.m
+++ recebe_velocidade.m
@@ -1,4 +1,4 @@
- function ler = recebe_velocidade
+ function vel = recebe_velocidade
 
   global SerPic
   pkg load instrument-control  %Carrega a bilbioteca que contem as funções de comunicação serial
@@ -7,26 +7,27 @@
   flushinput(SerPic);  %Limpa a porta serial de entrada
   fprintf(SerPic,'%c','w');
 
-  ler = srl_read(SerPic,2);%fscanf(SerPic, '%s');  %lendo os dados no PIC
- disp(num2str(ler))
-  %vel = str2double(ler);
+  % 5 é o numero de Bytes lidos pela função, cada byte é um caractere
+  [read_data,count] = fread(SerPic, 5); %lendo os dados no PIC;
 
-##
-##  if vel > 80 % Checagem de erro se velocidade é positiva
-##    fprintf(SerPic,'%c','w');
-##    ler = fscanf(SerPIC,'%s');
-##    vel = str2double(ler);
-##  endif
-##  if vel<0 %Checagem de erro se velocidade é negativa
-##       fprintf(SerPic,'%c','w');
-##       ler = fscanf(SerPIC,'%s');
-##       vel = str2double(ler);
-##  endif
-##  if isnan(vel) %Checagem de erro se velocidade e NaN
-##       fprintf(SerPic,'%c','w');
-##       ler = fscanf(SerPIC,'%s');
-##       vel = str2double(ler);
-##  endif
+  vel = str2double(char(read_data)); % tranforma os dadso em Double
+
+
+  if vel > 80 % Checagem de erro se velocidade é positiva
+    fprintf(SerPic,'%c','w');
+    [read_data,count] = fread(SerPic, 5);
+    vel = str2double(read_data);
+  endif
+  if vel<0 %Checagem de erro se velocidade é negativa
+       fprintf(SerPic,'%c','w');
+       read_data = fscanf(SerPIC,'%s');
+       vel = str2double(char(read_data));
+  endif
+  if isnan(vel) %Checagem de erro se velocidade e NaN
+       fprintf(SerPic,'%c','w');
+       read_data = fscanf(SerPIC,'%s');
+       vel = str2double(char(read_data));
+  endif
 
   flushoutput(SerPic);
   end
